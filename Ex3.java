2de222e2f857cc08b7e4adc16765051ee722585d
KreoX <soldimksol@gmail.com>
2022-12-03 13:24:54 +0300

    Ex3

---
+++ Ex3.java
@@ -0,0 +1,22 @@
+public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("aa"));
+
+    }
+
+    public static Boolean isPalindrome(String str) {
+        char[] charAr = str.toCharArray();
+        int len = 0;
+        if (charAr.length % 2 == 0) {
+            len = charAr.length / 2 - 1;
+        } else {
+            len = charAr.length / 2;
+        }
+        for (int index = 0; index <= len; index++) {
+            if (charAr[index] != charAr[charAr.length - 1 - index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
